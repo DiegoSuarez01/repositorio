97898d7f2b5e4e94f2b48e68a02122e42d3110e9
TRABAJOS_DE_GRADO_CYT <dasuarezb@hotmail.com>
2025-08-01 12:50:39 -0500

    cambios settings

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'crispy_forms',  # Agregar esta linea
+    'crispy_forms',  
     'crispy_bootstrap4',
-    'repositorio',                                    
+    'repositorio',    
+    
+    'storages',                                
 ]
 
+# AWS S3 Config
 
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = 'repositorio-dte'
+AWS_S3_REGION_NAME = 'us-east-2'  # O tu región
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False  # Para hacer los archivos públicos (sin firmas)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
